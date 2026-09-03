9705547779b559e26f599543ff4177a7994e72ed
cnerylozada <cnerylozada@gmail.com>
2026-09-03 17:08:58 -0500

    feat: gluestack setup

--- app/(drawer)/home.tsx
+++ app/(drawer)/home.tsx
@@ -1,12 +1,24 @@
 import { ScreenLayout } from "@/components/ScreenLayout";
 import { ThemedText } from "@/components/ThemedText";
-import { View } from "react-native";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <ScreenLayout>
       <View className="flex-1">
         <ThemedText>HomeScreen </ThemedText>
+        <View>
+          <Text className="text-red-400">Lucciano</Text>
+        </View>
+
+        <Button variant="outline">
+          <ButtonText>Press me</ButtonText>
+        </Button>
+
+        <Button variant="outline">
+          <ButtonText>Pay your debts!</ButtonText>
+        </Button>
       </View>
     </ScreenLayout>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { AppProviders } from "@/providers/AppProviders";
 import {
   DarkTheme,
@@ -6,21 +7,25 @@ import {
 } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "react-native";
+import { useColorScheme } from "nativewind";
 import "react-native-reanimated";
 import "./global.css";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const { colorScheme } = useColorScheme();
 
   return (
     <AppProviders>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <StatusBar style="auto" />
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(drawer)" />
-        </Stack>
-      </ThemeProvider>
+      <GluestackUIProvider mode="system">
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <StatusBar style="auto" />
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(drawer)" />
+          </Stack>
+        </ThemeProvider>
+      </GluestackUIProvider>
     </AppProviders>
   );
 }
